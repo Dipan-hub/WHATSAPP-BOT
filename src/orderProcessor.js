3aa495ea431ec05cf1e01dbc708dab4982a84270
Dipan <ms21btch11021.iith.ac.in>
2025-02-04 14:53:51 +0530

    Correct Payment Breakdown : Test 11

--- src/orderProcessor.js
+++ src/orderProcessor.js
@@ -137,7 +137,7 @@ async function extractOrderDetails(message) {
   // As described: finalPicapoolPrice = sumSalePrice * 1.05 + DELIVERY_FEE
   // Where 1.05 means 5% tax on sumSalePrice
   const taxAmount = sumSalePrice * TAX_RATE;
-  const finalPicapoolPrice = sumSalePrice + taxAmount + DELIVERY_FEE;
+  const finalPicapoolPrice = (sumSalePrice + DELIVERY_FEE) * (1+TAX_RATE);
 
   console.log("[extractOrderDetails] totalAfterDiscounts  =", totalAfterDiscounts);
   console.log("[extractOrderDetails] sumSalePrice (total discounted) =", sumSalePrice);
