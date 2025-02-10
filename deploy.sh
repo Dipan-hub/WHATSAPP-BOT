2ce63f5590a1ed047a1dc980983e4a7ac1ab263e
Dipan <ms21btch11021.iith.ac.in>
2025-02-10 13:12:19 +0530

    Removed Aashish Contact

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Ask for commit message
+echo "Enter commit message: "
+read commit_message
+
+# Execute the Git commands
+git add .
+git commit -m "$commit_message"
+git push origin main
+git push heroku main
+heroku logs --tail --app heroku-whatsapp-bot
