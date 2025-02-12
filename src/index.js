18df707c2103a1793cdc89315436aefc475e8a73
Dipan <ms21btch11021.iith.ac.in>
2025-02-12 08:23:31 +0530

    Added 20 Mesage

--- src/index.js
+++ src/index.js
@@ -131,7 +131,7 @@ Your order has been received.`;
       const msgBody = message.text?.body;
 
       // For non-admin senders, check if we've reached today's order limit
-      
+      /*
       if (true) {
         forwardMessageToAdmin(from, msgBody);
         //const orderLimitMsg = "We have reached today's order limit of 20 orders, please come back soon!";
@@ -145,7 +145,7 @@ Your order has been received.`;
         await sendMessage(from, orderLimitMsg);
         return res.sendStatus(200);
       }
-  
+  */
       if (from === ADMIN_NUMBER) {
         // Admin logic: parse admin response for direct replies
         const responseParts = msgBody.split(" - ");
